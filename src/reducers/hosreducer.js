9b3885ae27d52cbd2e899e2d711ddf5d10c122c5
ymozi <1669519865@qq.com>
2018-11-30 22:03:06 +0800

    后台接口-医院信息/医生信息连接

--- src/reducers/hosreducer.js
+++ src/reducers/hosreducer.js
@@ -1,9 +1,11 @@
-export default function hosreducer(state={list:null},actions){
+export default function hosreducer(state={list:null,hopital:null},actions){
     switch(actions.type){
-        case 'hosAjax':
-        console.log(actions.data);
-        console.log("调用了hosreducer",actions.data);
+        case 'doclist':
+        // console.log(actions);
+        // console.log("调用了hosreducer",actions.data);
         return ({},state,{list:actions.data});
+        case 'hospitalmsg':
+        return ({},state,{hospital:actions.hosMsg});
         default:
         return state;
     }
